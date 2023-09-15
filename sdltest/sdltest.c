6f4e3918918b555f5afef0ee991fa32a7373cb5e
Ulf Dellbrügge <ulfdellbruegge@gmail.com>
2023-09-16 00:29:12 +0200

    remove obsolete comments

--- sdltest/sdltest.c
+++ sdltest/sdltest.c
@@ -1,13 +1,9 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2023
-and may not be redistributed without written permission.*/
-
-// Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 
-// Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
 enum boolean
 {
     FALSE,
@@ -16,20 +12,13 @@ enum boolean
 
 int main(int argc, char *args[])
 {
-    // The window we'll be rendering to
-    SDL_Window *window = NULL;
-
-    // The surface contained by the window
-    SDL_Surface *screenSurface = NULL;
-
-    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
         return 1;
     }
    
-    // Create window
+    SDL_Window *window = NULL;
     window = SDL_CreateWindow("HELLO SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL)
     {
@@ -37,13 +26,11 @@ int main(int argc, char *args[])
         return 1;
     }
   
-    // Get window surface
+    SDL_Surface *screenSurface = NULL;
     screenSurface = SDL_GetWindowSurface(window);
 
-    // Fill the surface white
     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-    // Update the surface
     SDL_UpdateWindowSurface(window);
 
     // Hack to get window to stay up
@@ -58,10 +45,8 @@ int main(int argc, char *args[])
         }
     }
   
-    // Destroy window
     SDL_DestroyWindow(window);
 
-    // Quit SDL subsystems
     SDL_Quit();
 
     return 0;
