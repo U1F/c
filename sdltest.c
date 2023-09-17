4dd3c99169d6297ce912674c2d0767841e7a7324
Ulf Dellbrügge <ulfdellbruegge@gmail.com>
2023-09-17 10:13:54 +0200

    split to header

--- sdltest.c
+++ sdltest.c
@@ -1,36 +1,7 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <stdio.h>
-
-typedef struct {
-  int full_screen;
-  int resolution_width;
-  int resolution_height;
-  int vsync;
-  int anti_aliasing;
-  char *texture_quality;
-  char *shader_quality;
-} ConfigGraphics;
-
-typedef struct {
-  int master_volume;
-  int music_volume;
-  int sfx_volume;
-  int voice_volume;
-  int ambience_volume;
-  int mute_all;
-  char *audio_output;
-} ConfigAudio;
-
-typedef struct {
-  int grid_size;
-  int show_grid;
-  int auto_save_interval;
-  int undo_stack_size;
-  int default_layer;
-  int snap_to_grid;
-  char *tileset_path;
-} ConfigEditorSettings;
+#include "sdltest.h"
 
 ConfigGraphics config_graphics;
 ConfigAudio config_audio;
@@ -42,28 +13,9 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int ASSET_AREA_WIDTH = 200;
 
-typedef struct  {
-  int x;
-  int y;
-} Point;
-
-typedef struct  {
-  int width;
-  int height;
-} Dimensions;
 
 const Point TOP_LEFT = {0, 0};
 
-typedef struct  {
-  Point position;
-  Dimensions dimensions;
-} Rect;
-
-typedef struct  {
-  Uint8 red;
-  Uint8 green;
-  Uint8 blue;
-} Color;
 
 const Color WHITE = {0xFF, 0xFF, 0xfF};
 const Color BLACK = {0x00, 0x00, 0x00};
@@ -74,52 +26,6 @@ const Color YELLOW = {0xFF, 0xFF, 0x00};
 const Color CYAN = {0x00, 0xFF, 0xFF};
 const Color MAGENTA = {0xFF, 0x00, 0xFF};
 
-/*
- * Parse settings file.
- *
- * Return value: 0 if the file was parsed successfully, 1 otherwise.
- */
-int parse_settings_file();
-
-/*
- * Handle events.
- *
- * e: event
- * renderer: renderer
- *
- * Return value: 0 if the program should not be terminated, 1 otherwise.
- */
-int handle_event(SDL_Event, SDL_Renderer *);
-
-/**
- * Free resources.
- *
- * renderer: renderer
- * window: window
- *
- * Return value: none
- */
-void free_resources_renderer(SDL_Renderer *, SDL_Window *);
-
-/**
- * Free resources.
- *
- * window: window
- *
- * Return value: none
- */
-void free_resources_window(SDL_Window *);
-
-/*
- * Main function.
- *
- * Arguments are not evaluated yet.
- *
- * argc: number of arguments
- * args: array of arguments
- *
- * Return value: 0 if the program was terminated successfully, 1 otherwise.
- */
 int main(int argc, char *argv[]) {
   if (argc == 2) {
     printf("The program was called with the following argument: %s\n", argv[1]);
---
+++ sdltest.h
@@ -0,0 +1,102 @@
+#include <SDL2/SDL.h>
+
+typedef struct {
+  int full_screen;
+  int resolution_width;
+  int resolution_height;
+  int vsync;
+  int anti_aliasing;
+  char *texture_quality;
+  char *shader_quality;
+} ConfigGraphics;
+
+typedef struct {
+  int master_volume;
+  int music_volume;
+  int sfx_volume;
+  int voice_volume;
+  int ambience_volume;
+  int mute_all;
+  char *audio_output;
+} ConfigAudio;
+
+typedef struct {
+  int grid_size;
+  int show_grid;
+  int auto_save_interval;
+  int undo_stack_size;
+  int default_layer;
+  int snap_to_grid;
+  char *tileset_path;
+} ConfigEditorSettings;
+
+
+typedef struct  {
+  int x;
+  int y;
+} Point;
+
+typedef struct  {
+  int width;
+  int height;
+} Dimensions;
+
+
+typedef struct  {
+  Point position;
+  Dimensions dimensions;
+} Rect;
+
+typedef struct  {
+  Uint8 red;
+  Uint8 green;
+  Uint8 blue;
+} Color;
+
+
+/*
+ * Parse settings file.
+ *
+ * Return value: 0 if the file was parsed successfully, 1 otherwise.
+ */
+int parse_settings_file();
+
+/*
+ * Handle events.
+ *
+ * e: event
+ * renderer: renderer
+ *
+ * Return value: 0 if the program should not be terminated, 1 otherwise.
+ */
+int handle_event(SDL_Event, SDL_Renderer *);
+
+/**
+ * Free resources.
+ *
+ * renderer: renderer
+ * window: window
+ *
+ * Return value: none
+ */
+void free_resources_renderer(SDL_Renderer *, SDL_Window *);
+
+/**
+ * Free resources.
+ *
+ * window: window
+ *
+ * Return value: none
+ */
+void free_resources_window(SDL_Window *);
+
+/*
+ * Main function.
+ *
+ * Arguments are not evaluated yet.
+ *
+ * argc: number of arguments
+ * args: array of arguments
+ *
+ * Return value: 0 if the program was terminated successfully, 1 otherwise.
+ */
