a3dbf36cc83cc489db33ad3151855ccdb94215c5
Ulf Dellbrügge <ulfdellbruegge@gmail.com>
2023-09-17 10:05:56 +0200

    add config parser

--- sdltest.c
+++ sdltest.c
@@ -2,35 +2,84 @@
 #include <SDL_image.h>
 #include <stdio.h>
 
-const int EIGHTH_WIDTH = 160;
-const int EIGHTH_HEIGHT = 90;
-const int QUARTER_WIDTH = EIGHTH_WIDTH * 2;
-const int QUARTER_HEIGHT = EIGHTH_HEIGHT * 2;
-const int HALF_WIDTH = QUARTER_WIDTH * 2;
-const int HALF_HEIGHT = QUARTER_HEIGHT * 2;
-const int SCREEN_WIDTH = HALF_WIDTH * 2;
-const int SCREEN_HEIGHT = HALF_HEIGHT * 2;
+typedef struct {
+  int full_screen;
+  int resolution_width;
+  int resolution_height;
+  int vsync;
+  int anti_aliasing;
+  char *texture_quality;
+  char *shader_quality;
+} ConfigGraphics;
 
-const int TOP_LEFT = 0;
-const int ASSET_AREA_WIDTH = EIGHTH_WIDTH * 3;
-const int EDITOR_AREA_WIDTH = EIGHTH_WIDTH * 5;
+typedef struct {
+  int master_volume;
+  int music_volume;
+  int sfx_volume;
+  int voice_volume;
+  int ambience_volume;
+  int mute_all;
+  char *audio_output;
+} ConfigAudio;
 
-struct Color {
+typedef struct {
+  int grid_size;
+  int show_grid;
+  int auto_save_interval;
+  int undo_stack_size;
+  int default_layer;
+  int snap_to_grid;
+  char *tileset_path;
+} ConfigEditorSettings;
+
+ConfigGraphics config_graphics;
+ConfigAudio config_audio;
+ConfigEditorSettings config_editor_settings;
+
+const int MIN_RESOLUTION_WIDTH = 800;
+const int MIN_RESOLUTION_HEIGHT = 600;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int ASSET_AREA_WIDTH = 200;
+
+typedef struct  {
+  int x;
+  int y;
+} Point;
+
+typedef struct  {
+  int width;
+  int height;
+} Dimensions;
+
+const Point TOP_LEFT = {0, 0};
+
+typedef struct  {
+  Point position;
+  Dimensions dimensions;
+} Rect;
+
+typedef struct  {
   Uint8 red;
   Uint8 green;
   Uint8 blue;
-};
+} Color;
 
-struct Color white = {0xFF, 0xFF, 0xfF};
-struct Color black = {0x00, 0x00, 0x00};
-struct Color red = {0xFF, 0x00, 0x00};
+const Color WHITE = {0xFF, 0xFF, 0xfF};
+const Color BLACK = {0x00, 0x00, 0x00};
+const Color RED = {0xFF, 0x00, 0x00};
+const Color GREEN = {0x00, 0xFF, 0x00};
+const Color BLUE = {0x00, 0x00, 0xFF};
+const Color YELLOW = {0xFF, 0xFF, 0x00};
+const Color CYAN = {0x00, 0xFF, 0xFF};
+const Color MAGENTA = {0xFF, 0x00, 0xFF};
 
-struct Color green = {0x00, 0xFF, 0x00};
-struct Color blue = {0x00, 0x00, 0xFF};
-
-struct Color yellow = {0xFF, 0xFF, 0x00};
-struct Color cyan = {0x00, 0xFF, 0xFF};
-struct Color magenta = {0xFF, 0x00, 0xFF};
+/*
+ * Parse settings file.
+ *
+ * Return value: 0 if the file was parsed successfully, 1 otherwise.
+ */
+int parse_settings_file();
 
 /*
  * Handle events.
@@ -40,7 +89,7 @@ struct Color magenta = {0xFF, 0x00, 0xFF};
  *
  * Return value: 0 if the program should not be terminated, 1 otherwise.
  */
-int handleEvent(SDL_Event, SDL_Renderer *);
+int handle_event(SDL_Event, SDL_Renderer *);
 
 /**
  * Free resources.
@@ -76,6 +125,15 @@ int main(int argc, char *argv[]) {
     printf("The program was called with the following argument: %s\n", argv[1]);
   }
 
+  config_graphics.full_screen = 0;
+  config_graphics.resolution_width = MIN_RESOLUTION_WIDTH;
+  config_graphics.resolution_height = MIN_RESOLUTION_HEIGHT;
+  
+  int settings_file_parsing_failed = parse_settings_file();
+  if (settings_file_parsing_failed) {
+    return 1;
+  }
+
   int initialization_failed = SDL_Init(SDL_INIT_VIDEO) < 0;
   if (initialization_failed) {
     printf("Error while initializing SDL: %s\n", SDL_GetError());
@@ -94,7 +152,7 @@ int main(int argc, char *argv[]) {
   SDL_Window *window = NULL;
   window = SDL_CreateWindow(
       "HELLO SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-      SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+      config_graphics.resolution_width, config_graphics.resolution_height, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
   if (window == NULL) {
     printf("Error while creating SDL_Window: %s\n", SDL_GetError());
     SDL_Quit();
@@ -108,7 +166,8 @@ int main(int argc, char *argv[]) {
     free_resources_window(window);
     return 1;
   }
-  SDL_SetWindowMinimumSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  SDL_SetWindowMinimumSize(window, MIN_RESOLUTION_WIDTH, MIN_RESOLUTION_HEIGHT);
   SDL_Texture *your_image_texture = IMG_LoadTexture(renderer, "your_image.png");
   if (your_image_texture == NULL) {
     printf("Unable to create texture! SDL_image Error: %s\n", IMG_GetError());
@@ -119,20 +178,21 @@ int main(int argc, char *argv[]) {
   SDL_Event e;
   int quit = 0;
   while (quit == 0) {
-    SDL_SetRenderDrawColor(renderer, magenta.red, magenta.green, magenta.blue,
+    SDL_SetRenderDrawColor(renderer, MAGENTA.red, MAGENTA.green, MAGENTA.blue,
                            0xFF);
     SDL_RenderClear(renderer);
 
     while (SDL_PollEvent(&e)) {
-      quit = handleEvent(e, renderer);
+      quit = handle_event(e, renderer);
     } // End of: while (SDL_PollEvent(%e))
 
-    // In your main loop, before calling SDL_RenderPresent
-    SDL_SetRenderDrawColor(renderer, cyan.red, cyan.green, cyan.blue, 0xFF);
-    SDL_Rect fillRect = {TOP_LEFT, TOP_LEFT, EDITOR_AREA_WIDTH, SCREEN_HEIGHT};
-    SDL_RenderFillRect(renderer, &fillRect);
+    SDL_SetRenderDrawColor(renderer, CYAN.red, CYAN.green, CYAN.blue, 0xFF);
+    const SDL_Rect RECT_EDITOR = {TOP_LEFT.x, TOP_LEFT.y, ASSET_AREA_WIDTH,
+                                  SCREEN_HEIGHT};
 
-    SDL_RenderCopy(renderer, your_image_texture, NULL, NULL);
+    SDL_RenderFillRect(renderer, &RECT_EDITOR);
+    SDL_RenderCopy(renderer, your_image_texture, NULL, &RECT_EDITOR);
+
     // Update screen
     SDL_RenderPresent(renderer);
   } // End of: while (quit == 0)
@@ -144,20 +204,90 @@ int main(int argc, char *argv[]) {
   return 0;
 } // End of: main function
 
-int handleEvent(SDL_Event e, SDL_Renderer *renderer) {
-  int shouldQuit = 0;
+int parse_settings_file() {
+  FILE *file = fopen("settings.ini", "r");
+  if (file == NULL) {
+    printf("Could not open file.\n");
+    return 1;
+  }
+
+  char line[256];
+  char *key, *value;
+  while (fgets(line, sizeof(line), file)) {
+    if (line[0] == '[' || line[0] == '\n') {
+      continue; // Skip sections and empty lines
+    }
+
+    key = strtok(line, "=");
+    value = strtok(NULL, "\n");
+
+    if (key && value) {
+      printf("Key: %s, Value: %s\n", key, value);
+      if (strcmp (key, "FullScreen") == 0) {
+        printf("FullScreen: %s\n", value);
+        config_graphics.full_screen = strcmp(value, "true") == 0;
+      } else if ( strcmp (key, "ResolutionWidth") == 0) {
+        config_graphics.resolution_width = atoi(value);
+      } else if ( strcmp (key, "ResolutionHeight") == 0) {
+        config_graphics.resolution_height = atoi(value);
+      } else if ( strcmp (key, "VSync") == 0) {
+        config_graphics.vsync = strcmp(value, "true") == 0;
+      } else if ( strcmp (key, "AntiAliasing") == 0) {
+        config_graphics.anti_aliasing = atoi(value);
+      } else if ( strcmp (key, "TextureQuality") == 0) {
+        config_graphics.texture_quality = strdup(value);
+      } else if ( strcmp (key, "ShaderQuality") == 0) {
+        config_graphics.shader_quality = strdup(value);
+      } else if ( strcmp (key, "MasterVolume") == 0) {
+        config_audio.master_volume = atoi(value);
+      } else if ( strcmp (key, "MusicVolume") == 0) {
+        config_audio.music_volume = atoi(value);
+      } else if ( strcmp (key, "SFXVolume") == 0) {
+        config_audio.sfx_volume = atoi(value);
+      } else if ( strcmp (key, "VoiceVolume") == 0) {
+        config_audio.voice_volume = atoi(value);
+      } else if ( strcmp (key, "AmbienceVolume") == 0) {
+        config_audio.ambience_volume = atoi(value);
+      } else if ( strcmp (key, "MuteAll") == 0) {
+        config_audio.mute_all = strcmp(value, "true") == 0;
+      } else if ( strcmp (key, "AudioOutput") == 0) {
+        config_audio.audio_output = strdup(value);
+      } else if ( strcmp (key, "GridSize") == 0) {
+        config_editor_settings.grid_size = atoi(value);
+      } else if ( strcmp (key, "ShowGrid") == 0) {
+        config_editor_settings.show_grid = strcmp(value, "true") == 0;
+      } else if ( strcmp (key, "AutoSaveInterval") == 0) {
+        config_editor_settings.auto_save_interval = atoi(value);
+      } else if ( strcmp (key, "UndoStackSize") == 0) {
+        config_editor_settings.undo_stack_size = atoi(value);
+      } else if ( strcmp (key, "DefaultLayer") == 0) {
+        config_editor_settings.default_layer = atoi(value);
+      } else if ( strcmp (key, "SnapToGrid") == 0) {
+        config_editor_settings.snap_to_grid = strcmp(value, "true") == 0;
+      } else if ( strcmp (key, "TilesetPath") == 0) {
+        config_editor_settings.tileset_path = strdup(value);
+      }
+    }
+  }
+
+  fclose(file);
+  return 0;
+} // End of: parse_settings_file function
+
+int handle_event(SDL_Event e, SDL_Renderer *renderer) {
+  int should_quit = 0;
   switch (e.type) {
   case SDL_QUIT:
     printf("The app was terminated by the user.\n");
-    shouldQuit = 1;
+    should_quit = 1;
     break;
   case SDL_WINDOWEVENT:
     if (e.window.event == SDL_WINDOWEVENT_RESIZED) {
-      int newWidth = e.window.data1;
-      int newHeight = e.window.data2;
-      if (newWidth > 0 && newHeight > 0) { // Only act on valid dimensions
-        SDL_RenderSetLogicalSize(renderer, newWidth, newHeight);
-        printf("newWidth: %d, newHeight: %d\n", newWidth, newHeight);
+      int new_width = e.window.data1;
+      int new_height = e.window.data2;
+      if (new_width > 0 && new_height > 0) { // Only act on valid dimensions
+        SDL_RenderSetLogicalSize(renderer, new_width, new_height);
+        printf("newWidth: %d, newHeight: %d\n", new_width, new_height);
       }
     }
     break;
@@ -176,7 +306,7 @@ int handleEvent(SDL_Event e, SDL_Renderer *renderer) {
   default:
     break;
   }
-  return shouldQuit;
+  return should_quit;
 } // End of: handleEvent function
 
 void free_resources_renderer(SDL_Renderer *renderer, SDL_Window *window) {
